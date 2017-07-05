82212f179722ceee2a01f305e5fd7a233a674b77
Vincent B <evilznet@gmail.com>
2017-07-05 11:12:58 +0200

    Add binary literal in checkout

--- InCsharp1/Order/CheckoutService.cs
+++ InCsharp1/Order/CheckoutService.cs
@@ -4,12 +4,34 @@ namespace CsharpEvolve.Order
 {
     public static class CheckoutService {
 
-        public static async Task<CreditCardOrder> CheckoutWithCreditCard(this ShoppingCart shoppingCart, CreditCardInfo cardInfo)
+        // should be external service
+        private static (bool isValid, double discount) IsValidDiscountCode(this ShoppingCart shoppingCart,  string dicountCode)
         {
+            double DiscountCalc(double amount)
+            {
+                return amount * .05;
+            }   
+            const int binaryMask = 0b1011_1100_1011_0011;
+            const int binaryValidator = 0b0000_0000_0000_0011;
+            var isValid =(dicountCode.GetHashCode() & binaryMask) == binaryValidator;
+
+            return isValid
+                ? (true, DiscountCalc(shoppingCart.TotalAmount.cartTotal))
+                : (false, 0);
+        }
+
+        public static async Task<CreditCardOrder> CheckoutWithCreditCard(this ShoppingCart shoppingCart, CreditCardInfo cardInfo, string discountCode)
+        {
+            (var isValid, var dicount) = shoppingCart.IsValidDiscountCode(discountCode);
+            if (isValid)
+            {
+               // apply discount in order ...
+            }
+
             await Task.Delay(3000); // simulate Card info verification
             return new CreditCardOrder(shoppingCart, cardInfo);
         }
-        public static async Task<PaypalOrder> CheckoutWithCreditCard(this ShoppingCart shoppingCart, PaypalInfo paypalInfo)
+        public static async Task<PaypalOrder> CheckoutWithCreditCard(this ShoppingCart shoppingCart, PaypalInfo paypalInfo, string discountCode)
         {
             await Task.Delay(3000); // simulate pappal info verification
             return new PaypalOrder(shoppingCart, paypalInfo);
